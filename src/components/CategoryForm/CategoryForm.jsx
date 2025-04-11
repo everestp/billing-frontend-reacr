3ea981637907d606468e923a2125e05e131da7d4
Everest Paudel <paudeleverest09@gmail.com>
2025-04-11 15:44:14 +0545

    Feat: add CategoryForm and CategoryList components to ManageCategory page

---
+++ src/components/CategoryForm/CategoryForm.jsx
@@ -0,0 +1,47 @@
+
+
+const CategoryForm = () => {
+  return (
+   <div className="mx-2 mt-2">
+<div className="row">
+    <div className="card col-md-8 form-container">
+        <div className="card-body">
+            <form >
+                <div className="mb-3">
+                    <label htmlFor="image" className="form-label">
+                        <img src="https://placehold.co/48x48" alt="" width={48} />
+                    </label>
+                    <input type="file" name="image" id="image" className="form-control" hidden />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label">Name</label>
+                    <input type="text" name="name" id="name" className="form-control" placeholder="Categoy Name"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="description" className="form-label">Description</label>
+                    <textarea rows={5} name="description" id="description" className="form-control" placeholder="Wite Content Here...."/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="bgcolor" className="form-lable">
+                        Backgroung color
+
+                    </label>
+                    <br />
+                    <input type="color"
+                     name="bgCollo"
+                      id="bgColor"
+                      placeholder="#ffffff"
+                       />
+                </div>
+
+<button type="submit" className="btn btn-primary w-100">Save</button>
+            </form>
+        </div>
+
+    </div>
+</div>
+   </div>
+  )
+}
+
+export default CategoryForm
--- src/pages/ManageCategory/ManageCategory.jsx
+++ src/pages/ManageCategory/ManageCategory.jsx
@@ -1,13 +1,15 @@
 
+import CategoryForm from "../../components/CategoryForm/CategoryForm"
+import CategoryList from "../../components/CategoryList/CategoryList"
 import "./ManageCategory.css"
 const ManageCategory = () => {
   return (
     <div className='category-container text-light'>
 <div className="left-column">
-category form
+<CategoryForm/>
 </div>
 <div className="right-column">
-  list of categories
+ <CategoryList/>
 </div>
     </div>
   )
