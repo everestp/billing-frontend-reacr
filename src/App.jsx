46fbf44f41a423402d4b94d012eecb2671d5bd17
Everest Paudel <paudeleverest09@gmail.com>
2025-04-11 13:00:40 +0545

    Add Menubar component and pages for Dashboard, Explore, Manage Items, Manage Categories, and Manage Users
    - Created Menubar component with navigation links and styles.
    - Added Dashboard, Explore, ManageCategory, ManageItems, and ManageUsers pages with basic structure.
    - Included CSS for Menubar styling.

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Menubar from './components/Menubar/Menubar'
 
 function App() {
   
   return (
     <>
      <div>
-      Helo world
+     <Menubar/>
      </div>
     </>
   )
---
+++ src/assets/assets.js
@@ -0,0 +1,8 @@
+
+import logo from './logo.png'
+import logo1 from './logo1.png'
+export const assests = {
+    logo,
+    logo1
+
+}
